69d6adcec157b3ff682a70f847087aa2d2e1c468
Dong-ik <ldigo@naver.com>
2025-11-17 15:43:29 +0900

    실습중

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { PropsAndState } from "./example/ProosAndState";
 import { StateComponent1 } from "./example/StateComponent1";
 import { StateComponent2 } from "./example/StateComponent2";
 import Hello from "./Hello";
 
 function App() {
   // return <Hello></Hello>;
-  return <StateComponent2></StateComponent2>;
+  return <PropsAndState></PropsAndState>;
+  // return <StateComponent1></StateComponent1>;
+  // return <StateComponent2></StateComponent2>;
 }
 
 export default App;
