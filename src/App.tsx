da3768e2c8d0f7c2df5012524ebebefd133f2c29
Dong-ik <ldigo@naver.com>
2025-11-17 16:32:37 +0900

    잘못된 Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { PropsAndState } from "./example/ProosAndState";
+import { PropsAndState } from "./example/PropsAndState";
 import { StateComponent1 } from "./example/StateComponent1";
 import { StateComponent2 } from "./example/StateComponent2";
 import Hello from "./example/Hello";
+import { NonContext } from "./example/Context/NonContext";
 
 function App() {
   // return <Hello></Hello>;
-  return <PropsAndState></PropsAndState>;
+  // return <PropsAndState></PropsAndState>;
   // return <StateComponent1></StateComponent1>;
   // return <StateComponent2></StateComponent2>;
+  return <NonContext></NonContext>;
 }
 
 export default App;
---
+++ src/example/Context/NonContext.tsx
@@ -0,0 +1,5 @@
+import { GrandParent } from "../../ContextExample/NonContext/GrandParent";
+
+export const NonContext = () => {
+  return <GrandParent value="Hello World" />;
+};
